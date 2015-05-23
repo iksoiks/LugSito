ded8729fd81d52801435b111f0545dd4b361e0a5
Lorenzo Pizzari <lorenzo-pizzari@users.noreply.github.com>
2015-05-23 12:50:23 +0100

    Update db_pages.js
    Fixate parentesi graffe

--- models/db_pages.js
+++ models/db_pages.js
@@ -1,7 +1,7 @@
 var orm = require("orm");
 
 module.exports = function (db, cb) {
-    var Page = db.define("page", {
+    var Page = db.define("pages", {
         title: String,
         description: String,
         text: String,
@@ -12,11 +12,12 @@ module.exports = function (db, cb) {
             //fullName: function () {
             //    return this.name + ' ' + this.surname;
             //}
-        },
+        }
+    }, {
         validations: {
-            title: orm.enforce.ranges.length(1, undefined, "Missing"),
-            description: orm.enforce.ranges.length(1, undefined, "Missing"),
-            text: orm.enforce.ranges.length(1, undefined, "Missing")
+            title: orm.enforce.notEmptyString(),
+            description: orm.enforce.notEmptyString(),
+            text: orm.enforce.notEmptyString()
         }
     });
 };
